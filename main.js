96a0c560e843126f96860d30f0bcd8113074bf9c
Davi Ferreira <daviferreiraaew@gmail.com>
2022-10-21 14:31:22 -0300

    Refactoring and adding new features

--- main.js
+++ main.js
@@ -1,35 +1,57 @@
 const screen1 = document.querySelector('.screen1')
 const screen2 = document.querySelector('.screen2')
-
-const randomNumber = Math.round(Math.random() * 10)
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
+//Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', backScreen)
+
+// funções callback
 function handleTryClick(event) {
   event.preventDefault() // não faça o padrão
 
   const inputNumber = document.querySelector('#inputNumber')
 
+  if (Number(inputNumber.value) < 0) {
+    alert('O número difitado não pode ser negativo')
+    xAttempts--
+  }
+  if (Number(inputNumber.value) > 10) {
+    alert('O número digitado não pode ser maior que 10')
+    xAttempts--
+  }
+  if (Number(inputNumber.value) == '') {
+    alert('Por favor digite um número')
+    xAttempts--
+  }
   if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add('hide')
-    screen2.classList.remove('hide')
+    toggleScreen()
 
-    document.querySelector(
-      '.screen2 h2'
+    screen2.querySelector(
+      'h2'
     ).innerText = `Você acertou em ${xAttempts} tentativas`
   }
   inputNumber.value = ''
   xAttempts++
 }
 
-//Eventos
-const btnTry = document.querySelector('#btnTry')
-const btnReset = document.querySelector('#btnReset')
-
-// função callback
-btnTry.addEventListener('click', handleTryClick)
-
-btnReset.addEventListener('click', function () {
-  screen1.classList.remove('hide')
-  screen2.classList.add('hide')
+function handleResetClick() {
+  toggleScreen()
   xAttempts = 1
-})
+  randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+  screen1.classList.toggle('hide')
+  screen2.classList.toggle('hide')
+}
+
+function backScreen(e) {
+  if (e.key == 'Enter' && screen1.classList.contains('hide')) {
+    handleResetClick()
+  }
+}
